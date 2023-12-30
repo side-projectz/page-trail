f5606f2cc7349ce0d31b5b158e5ad02c52dab77a
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-12-30 17:49:31 +0530

    minor: removed floating timer of each tab

--- dev/contentScript.ts
+++ dev/contentScript.ts
@@ -1,5 +1,3 @@
-// import Timer from "easytimer.js";
-
 function extractPageMetadata() {
   const metaTags = document.getElementsByTagName('meta');
   const metaData: {
@@ -42,34 +40,3 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
   return false;
 });
-
-// const div = document.createElement('div');
-// div.classList.add('page-trail-timer');
-// div.style.position = 'fixed';
-// div.style.bottom = '0';
-// div.style.right = '0';
-// div.style.backgroundColor = 'white';
-// div.style.padding = '10px';
-// div.style.border = '1px solid #ccc';
-// div.style.borderRadius = '5px';
-// div.style.zIndex = '99';
-
-// document.body.appendChild(div);
-
-// chrome.storage.local.get(['pageList'], function (result) {
-//   const pageList = result.pageList || [];
-//   const hostname = window.location.hostname;
-
-//   const domainMatchIndex = pageList.findIndex((item: any) => item.domain === hostname);
-//   const domain = domainMatchIndex > -1 ? pageList[domainMatchIndex] : null;
-
-//   const page = domain ? domain.pages.find((item: any) => item.page === window.location.href) : null;
-//   const pageTimer = page ? page.timeSpent : 0;
-
-//   const timer = new Timer({ precision: 'seconds' });
-//   timer.start();
-
-//   timer.addEventListener('secondsUpdated', function (e) {
-//     document.querySelector('.page-trail-timer')!.innerHTML = timer.getTimeValues().toString();
-//   });
-// });
