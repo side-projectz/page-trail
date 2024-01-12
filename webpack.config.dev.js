3532ef8bab921aadb0a99385c1fb17f827341e46
Surya Umapathy <suryaumapathy2812@gmail.com>
2024-01-12 16:55:26 +0530

    Add build:dev and postbuild:dev scripts, and create webpack.config.dev.js

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: "production",
+  entry: { 
+    background: './dev/background.ts',
+    contentScript: './dev/contentScript.ts',
+   },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [{
+          loader: 'ts-loader',
+          options: {
+            configFile: (__dirname + '/tsconfig.chrome.json') // Path to your custom tsconfig
+          }
+        }],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: (__dirname + '/dist')
+  },
+  optimization: {
+    minimize: false // This disables the minification
+  }
+}
