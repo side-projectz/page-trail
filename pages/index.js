37dbbdd4bad1c83342f7633f21d55efb9e4b87a3
Surya Umapathy <suryaumapathy2812@gmail.com>
2024-03-02 11:05:35 +0530

    major: auth using WebAuthFlow

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ import Listener from '../components/listener';
 
 // Move the API call outside of the component
 const fetchUserInfo = async () => {
-  const { token } = await chrome.identity.getAuthToken({ interactive: true });
+  const authResult = await launchWebAuthFlow_and_returnCallBackParams();
+  if (!authResult) {
+    return null;
+  }
+
+  const { token } = authResult;
   const response = await fetch(
     `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${token}`
   );
@@ -67,3 +72,32 @@ export default function Home() {
     </>
   );
 }
+
+const launchWebAuthFlow_and_returnCallBackParams = async () => {
+  const redirectURL = chrome.identity.getRedirectURL();
+  const { oauth2 } = chrome.runtime.getManifest();
+  const clientId = oauth2.client_id;
+  const scopes = oauth2.scopes.join(' ');
+
+  console.log('redirectURL', redirectURL);
+
+  const authParams = new URLSearchParams({
+    client_id: clientId,
+    redirect_uri: redirectURL,
+    response_type: 'token',
+    scope: scopes,
+  });
+
+  console.log('authParams', authParams.toString());
+  const authURL = `https://accounts.google.com/o/oauth2/v2/auth?${authParams.toString()}`;
+
+  const responseUrl = await chrome.identity.launchWebAuthFlow({
+    url: authURL,
+    interactive: true,
+  });
+  const url = new URL(responseUrl);
+  const urlParams = new URLSearchParams(url.hash.slice(1));
+  const { access_token } = Object.fromEntries(urlParams.entries());
+
+  return { token: access_token };
+};
